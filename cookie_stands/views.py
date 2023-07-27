a8be019936f8eb27ff1eb6b792c4d1a40661ead2
AnthonySinitsa <itonyemail@gmail.com>
2023-07-27 21:48:08 +0000

    command

--- cookie_stands/views.py
+++ cookie_stands/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from .serializers import CookieStandSerializer
+from .models import CookieStand
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class CookieStandList(ListCreateAPIView):
+    queryset = CookieStand.objects.all()
+    serializer_class = CookieStandSerializer
+    
+class CookieStandDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
+    queryset = CookieStand.objects.all()
+    serializer_class = CookieStandSerializer
